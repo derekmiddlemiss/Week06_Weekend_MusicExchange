5e66794066f1320b6a6b5b2b4effbe339fa7bf98
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-08-05 15:31:55 +0100

    Instrument tested via Guitar subclass, passing

--- music_exchange_management/Guitar.java
+++ music_exchange_management/Guitar.java
@@ -13,4 +13,6 @@ public class Guitar extends Instrument{
     return this.numStrings;
   }
 
+  
+
 }
---
+++ music_exchange_specs/InstrumentTest.java
@@ -0,0 +1,51 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import music_exchange_management.*;
+
+public class InstrumentTest{
+
+  Guitar fenderBender;
+
+  @Before
+  public void before(){
+    fenderBender = new Guitar( 300.00, 499.99, "Fender NoteBender21", "Beechwood", "Red", "String", "Thrum", 6 );
+  }
+
+  //Test two interface methods
+  @Test
+  public void testCanPlay(){
+    assertEquals( "Thrum", fenderBender.play() );
+  }
+
+  @Test
+  public void testCalculateMarkup(){
+    assertEquals( 199.99, fenderBender.calculateMarkup(), 0.001 );
+  }
+
+  //Test method inherited from Product
+  @Test
+  public void testGetIdentifier(){
+    assertEquals( "Fender NoteBender21", fenderBender.getIdentifier() );
+  }
+
+  @Test
+  public void testGetMaterial(){
+    assertEquals( "Beechwood", fenderBender.getMaterial() );
+  }
+
+  @Test
+  public void testGetColour(){
+    assertEquals( "Red", fenderBender.getColour() );
+  }
+
+  @Test
+  public void testGetType(){
+    assertEquals( "String", fenderBender.getType() );
+  }
+
+  @Test
+  public void testGetNumStrings(){
+    assertEquals( 6, fenderBender.getNumStrings() );
+  }
+
+}
